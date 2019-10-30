7998d34f0fb55e45f10ac5c4fd2c7190913a9aeb
Siddharth Bharadwaj <siddharthbharadwaj45@gmail.com>
2019-10-30 09:58:12 +0530

    Initial Import
    Initial import for prebuilt OrangeFox recovery.

---
+++ ofrp.mk
@@ -0,0 +1,4 @@
+OFRP_PATH := device/ofrp/jasmine_sprout
+
+TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(OFRP_PATH)/ramdisk-recovery.img
+TARGET_PREBUILT_RECOVERY_RAMDISK := $(OFRP_PATH)/ramdisk-recovery.zip
